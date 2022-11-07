b75dd89517510f60ee2912cdd1b587b04f8ba2f7
Juan Ercole <juan.m.ercole@gmail.com>
2022-11-07 15:23:15 -0300

    fin

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router-dom"
 import { LoginPage } from "../auth"
 import { HerosRoutes } from "../heros"
+import { PrivateRoute } from "./PrivateRoute"
+import { PublicRoute } from "./PublicRoute"
 
 
 
@@ -10,7 +12,18 @@ export const AppRouter = () => {
 
       <Routes>
 
-          <Route path="/login" element={ <LoginPage />}/>
+          <Route path="/login" element={
+             <PublicRoute>
+              <LoginPage/>
+             </PublicRoute> }
+            />
+          
+
+          <Route path="/*" element={ 
+            <PrivateRoute>
+              <HerosRoutes/>
+            </PrivateRoute>}
+          />
           
           <Route path="/*" element={ <HerosRoutes />}/>
           
